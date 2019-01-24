25562b266abab5459cb035b01e4af3e4bde0b16d
motto813 <hammerschmidtpaul@gmail.com>
2019-01-24 15:05:11 -0600

    check cli arguments for validity

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const args = process.argv.slice(2);
 const passwordGenerator = require('./generator').password;
 
 try {
-  if (
-    args.length !== 2
-    || typeof +args[0] !== 'number'
-    || typeof +args[1] !== 'number'
-  ) {
+  if (args.length !== 2 || typeof +args[0] !== 'number' || typeof +args[1] !== 'number') {
     throw new Error('must supply 2 number arguments');
+  } else if (+args[0] > +args[1]) {
+    throw new Error('min length cannot be greater than max length');
+  } else if (+args[0] < 8) {
+    throw new Error('min length must be 8 or more');
+  } else if (+args[1] > 40) {
+    throw new Error('max length must be 40 or less');
   }
   console.log('generated password:', passwordGenerator(+args[0], +args[1]));
 } catch (e) {
